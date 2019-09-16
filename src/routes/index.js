3b437e58f5b7c0e44a3970d3d12b7dbcf1f97f96
Elan Fraga <elanfraga@gmail.com>
2019-09-15 23:44:40 -0300

    Configurando rotas

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashborad';
+import Profile from '../pages/Profile';
+
+
+export default Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  );
+}
